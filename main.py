855ec9505e57c4604acc3ac58788905620bf452c
Deanna Liu <43284390+deannaliu@users.noreply.github.com>
2021-08-28 05:30:19 +0000

    : generate inspirational quote

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import discord #dicord.py library - asynch - callback
 import os
+import requests #allows code to make http req to get data from API - returns JSON
+import json
+
 
 client = discord.Client()  #creates an instance of a client
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return(quote)
 
 @client.event #this is how you register event
 async def on_ready(): #when bot ready to start being used
@@ -13,5 +22,8 @@ async def on_message(message): #if msg from bot, don't do anything
     return # checks if message from the bot
   if message.content.startswith('$hello'): #command
     await message.channel.send('Hello Sheebers!') #response from bot
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
 
 client.run(os.getenv('TOKEN'))
